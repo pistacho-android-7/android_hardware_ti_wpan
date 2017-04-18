a665262bbcf3a23f5f869322175ec4e0addf1be4
Wig C <onlywig@gmail.com>
2017-04-19 00:20:31 +0800

    wg7833: ti bluetooth wpan HAL layer initialize.

---
+++ ti-wpan-products.mk
@@ -0,0 +1,28 @@
+# This file lists the firmware, software that are specific to
+# WiLink connectivity chip on OMAPx platforms.
+
+PRODUCT_PACKAGES += uim-sysfs \
+	kfmapp \
+	FmRxApp \
+	FmTxApp \
+	FmService \
+	libfmradio \
+	fmradioif \
+	com.ti.fm.fmradioif.xml \
+	libbt-vendor \
+	ti-wpan-fw \
+	BluetoothSCOApp
+
+#NFC
+PRODUCT_PACKAGES += \
+	libnfc \
+	libnfc_ndef \
+	libnfc_jni \
+	Nfc \
+	NFCDemo \
+	Tag \
+	TagTests \
+	TagCanon \
+	AndroidBeamDemo \
+	NfcExtrasTests \
+	com.android.nfc_extras
